8282072e0be3c2eed817033acae470075e498a35
Derek Etman <dereketman@gmail.com>
2020-02-12 08:27:07 -0800

    + Done

--- src/hashtable.py
+++ src/hashtable.py
@@ -1,40 +1,87 @@
 # '''
 # Linked List hash table key/value pair
 # '''
+
+
 class LinkedPair:
     def __init__(self, key, value):
         self.key = key
         self.value = value
         self.next = None
 
+    def __repr__(self):
+        return f"{self.key}, {self.value}, {self.next}"
+
+
+class LinkedList:
+    def __init__(self, head=None, next_node=None):
+        self.head = head
+        self.next = next_node
+
+    def add_to_head(self, node):
+        if self.head is None:
+            self.head = node
+
+        node.next = self.head
+        self.head = node
+
+    def find_key(self, key):
+        current = self.head
+
+        while current is not None:
+            if current.key == key:
+                return current.value
+            else:
+                current = current.next
+
+        return None
+
+    def remove(self, key):
+        if self.head.key == key:
+            self.head = self.head.next
+            return
+
+        current = self.head
+
+        while current.next is not None:
+            if current.next.key == key:
+                current.next = current.next.next
+            else:
+                current = current.next
+
+        return None
+
+
 class HashTable:
     '''
     A hash table that with `capacity` buckets
     that accepts string keys
     '''
+
     def __init__(self, capacity):
         self.capacity = capacity  # Number of buckets in the hash table
         self.storage = [None] * capacity
-        self.count = 0
-
 
     def _hash(self, key):
         '''
         Hash an arbitrary key and return an integer.
-
         You may replace the Python hash with DJB2 as a stretch goal.
         '''
-        return hash(key)
-
+        hashed = 0
+        prime = 31
+        for char in key:
+            hashed = prime * hashed + ord(char)
+        return hashed
 
     def _hash_djb2(self, key):
         '''
         Hash an arbitrary key using DJB2 hash
-
         OPTIONAL STRETCH: Research and implement DJB2
         '''
-        pass
-
+        hash = 5381
+        for x in key:
+            hash = ((hash << 5) + hash) + ord(x)
+        return hash & 0xFFFFFFFF
 
     def _hash_mod(self, key):
         '''
@@ -43,68 +90,70 @@ class HashTable:
         '''
         return self._hash(key) % self.capacity
 
-
     def insert(self, key, value):
         '''
         Store the value with the given key.
-
         Hash collisions should be handled with Linked List Chaining.
-
         Fill this in.
         '''
-        # checking if cap is smaller than count
-        if self.count >= self.capacity:
-            # make it bigger
-            self.resize()
+        index = self._hash_mod(key)
+        print(index)
 
-        # using hash mod to get index
-        index = self._hash_mod
+        new_pair = LinkedPair(key, value)
+        new_linked_list = LinkedList(new_pair)
 
-        if self.storage[index]:
-            if self.storage[index] and self.storage[index].key != key:
-                # make a new list
-                node = self.storage[index]
-                node.next = LinkedPair(key, value)
-            else:
-                self.stoarge[index].value = value
-        # if index in the storage is empty, set it
+        if self.storage[index] is None:
+            self.storage[index] = new_linked_list
         else:
-            self.storage[index] = LinkedPair(key, value)
-        self.count += 1
-
+            self.storage[index].add_to_head(new_pair)
 
     def remove(self, key):
         '''
         Remove the value stored with the given key.
-
         Print a warning if the key is not found.
+        Fill this in.
         '''
         index = self._hash_mod(key)
 
-        # if the key is in storage
-        if self.storage[index]:
-            #  change to None
-            self.storage[index] = None
-            self.count -= 1
+        if self.storage[index] is None:
+            return None
         else:
-            print(f"{key} doesnt exist.")
-
+            self.storage[index].remove(key)
 
     def retrieve(self, key):
         '''
         Retrieve the value stored with the given key.
-
         Returns None if the key is not found.
-
         Fill this in.
         '''
-        pass
+        index = self._hash_mod(key)
 
+        if self.storage[index] is None:
+            return None
+        else:
+            return self.storage[index].find_key(key)
 
     def resize(self):
-        index = self._hash_mod(key)
-        return (index + 1) % self.resize()
+        '''
+        Doubles the capacity of the hash table and
+        rehash all key/value pairs.
+        Fill this in.
+        '''
+        self.capacity *= 2
 
+        new_storage = [None] * self.capacity
+
+        old = self.storage[:]
+
+        self.storage = new_storage
+
+        for i in range(len(old)):
+            if old[i] is not None:
+                current = old[i].head
+
+                while current is not None:
+                    self.insert(current.key, current.value)
+                    current = current.next
 
 
 if __name__ == "__main__":
@@ -113,8 +162,6 @@ if __name__ == "__main__":
     ht.insert("line_1", "Tiny hash table")
     ht.insert("line_2", "Filled beyond capacity")
     ht.insert("line_3", "Linked list saves the day!")
-
-    print("")
 
     # Test storing beyond capacity
     print(ht.retrieve("line_1"))
@@ -129,6 +176,13 @@ if __name__ == "__main__":
     print(f"\nResized from {old_capacity} to {new_capacity}.\n")
 
     # Test if data intact after resizing
+    print(ht.retrieve("line_1"))
+    print(ht.retrieve("line_2"))
+    print(ht.retrieve("line_3"))
+
+    print("")
+
+    ht.remove("line_1")
     print(ht.retrieve("line_1"))
     print(ht.retrieve("line_2"))
     print(ht.retrieve("line_3"))
